18c635d0990f66bdda30525c1f000dbe9bbb1f7f
jibek abdyrazakova <jibekabdyrazakova@jibeks-MacBook-Air.local>
2026-01-15 10:01:17 +0600

    4
    5
    
    6

--- src/kg/sim_bank/promotion/JanuaryPromotion.java
+++ src/kg/sim_bank/promotion/JanuaryPromotion.java
@@ -14,6 +14,12 @@ public class JanuaryPromotion {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+
+
+
 
 
 
